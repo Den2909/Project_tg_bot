dea0f9623d46a65ea6ceb7ec8905ec10e52730e1
Denis Tryapitsyn <114642523+Den2909@users.noreply.github.com>
2025-06-09 15:44:31 +0500

    Update lint.sh

--- lint.sh
+++ lint.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-echo "Running Black..."
-black --check .
-echo "Running Flake8..."
-flake8 .
-echo "Running Pylint..."
-pylint *.py
-echo "Running Mypy..."
-mypy .
-echo "Linting completed!"
+#python -m pip install --upgrade pip
+#pip install black==24.8.0 flake8==7.1.1 pylint==3.3.1 mypy==1.11.2 pytest==8.3.3 pytest-asyncio==0.24.0 pytest-mock==3.14.0 
+python -m black .
+python -m black --check .
+python -m flake8 .
+python -m pylint *.py
+python -m mypy .
+python -m pytest tests/ -v --asyncio-mode=auto
